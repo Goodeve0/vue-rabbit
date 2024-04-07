55cb8dca619a5af6b572bcc86837e2b426553ae1
c <2655774991@qq.com>
2024-04-07 10:42:56 +0800

    退出登陆时清除购物车数据

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -53,7 +53,10 @@ export const useCartStore = defineStore('cart', () => {
             cartList.value.splice(idx, 1)
         }
     }
-
+    //清除购物车
+    const clearCart = () => {
+        cartList.value = []
+    }
     //单选功能
     const singleCheck = (skuId, selected) => {
         //通过skuId找到要修改的那一项，然后把它的selected修改为传过来的selected
@@ -86,7 +89,8 @@ export const useCartStore = defineStore('cart', () => {
         isAll,
         allCheck,
         selectedCount,
-        selectedPrice
+        selectedPrice,
+        clearCart
     }
 }, {
     persist: true
